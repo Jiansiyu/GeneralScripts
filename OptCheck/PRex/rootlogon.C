62cb53fc6926468c4db804b1bfc5f15ed434e16c
Siyu <jiansiyu@gmail.com>
2021-01-25 16:24:53 -0500

    change filen path

--- OptCheck/PRex/rootlogon.C
+++ OptCheck/PRex/rootlogon.C
@@ -1,19 +1,5 @@
 {
  	gStyle->SetPalette(1);
-    gROOT->LoadMacro("OpticsFocalVarCheck.C");
-// 	gROOT->LoadMacro("cutManual.C");	
-//	gROOT->LoadMacro("cutPro.C");
-// 	gROOT->LoadMacro("OpticsGraphicCutProPrex.C");
-//	gROOT->LoadMacro("RasterGraphicCutPro.C");
-//	gROOT->LoadMacro("OpticsGraphicCutProMultiPeakFit.C");
-//	gROOT->LoadMacro("OpticsCrossValidation.C");
-//	gROOT->LoadMacro("OpticsGraphicCutProH2O.C");
-//	gROOT->LoadMacro("OpticsPointingDpCheck.C");
-
-//	gROOT->LoadMacro("OpticsFocalVariableCheck.C");
-//	gROOT->LoadMacro("getCentralP.C");
-//	gROOT->LoadMacro("plotTemp.C");
-//	gROOT->LoadMacro("bpmCheck.C");
-//	gROOT->LoadMacro("septumCheck.C");
-//	gROOT->LoadMacro("TargetCheck.C");
+//    gROOT->LoadMacro("OpticsFocalVarCheck.C");
+// 	gROOT->LoadMacro("CheckChec.C");
 }
