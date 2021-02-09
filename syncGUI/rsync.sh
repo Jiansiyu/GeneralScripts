65974b27c71e7a1ec56142dda91fc4642ca93cfe
Siyu <jiansiyu@gmail.com>
2021-02-09 03:41:09 -0500

    auto sync bug fixed

--- syncGUI/rsync.sh
+++ syncGUI/rsync.sh
@@ -2,6 +2,9 @@
 
 Target=$1
 Destination=$2
-while inotifywait -r -e modify,create,delete,move ${Target}; do
+while inotifywait -r -e modify,create,delete,move ${Target}
+do
+    echo "file changed start sync!"
+    echo ${Target}
     rsync -avP --ignore-existing ${Target} ${Destination}
 done
