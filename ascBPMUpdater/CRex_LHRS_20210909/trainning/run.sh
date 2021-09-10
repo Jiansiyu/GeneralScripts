55bc19dfe853b7aedacc98c1b2bb4c8ab303b532
Siyu <jiansiyu@gmail.com>
2021-09-09 23:23:37 -0400

    add CRex training set +1 -1 0

---
+++ ascBPMUpdater/CRex_LHRS_20210909/trainning/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+../../trans /home/newdriver/pyQuant/CRex_Backups/LHRS/Cut20210909_CRex/cutfile/WithOutMomCut/Sieve._2684_p4.f51
+../../trans /home/newdriver/pyQuant/CRex_Backups/LHRS/Cut20210909_CRex/cutfile/WithOutMomCut/Sieve._2689_p4.f51
+../../trans /home/newdriver/pyQuant/CRex_Backups/LHRS/Cut20210909_CRex/cutfile/WithOutMomCut/Sieve._2718_p4.f51
