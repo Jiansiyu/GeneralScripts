970044f4a857667c772fa27373130a14153ed8e1
Siyu <jiansiyu@gmail.com>
2021-09-21 00:17:01 -0400

    add CRex RHRS

---
+++ ascBPMUpdater/CRex_RHRS_20210920/LargeDataset/run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+../../trans  /home/newdriver/pyQuant/CRex_Backups/RHRS/cutfile/LargeDataset/Sieve._21743_p4.f51
+../../trans  /home/newdriver/pyQuant/CRex_Backups/RHRS/cutfile/LargeDataset/Sieve._21744_p4.f51
+../../trans  /home/newdriver/pyQuant/CRex_Backups/RHRS/cutfile/LargeDataset/Sieve._21748_p4.f51
+../../trans  /home/newdriver/pyQuant/CRex_Backups/RHRS/cutfile/LargeDataset/Sieve._21749_p4.f51
+../../trans  /home/newdriver/pyQuant/CRex_Backups/RHRS/cutfile/LargeDataset/Sieve._21752_p4.f51
+../../trans  /home/newdriver/pyQuant/CRex_Backups/RHRS/cutfile/LargeDataset/Sieve._21753_p4.f51
+../../trans  /home/newdriver/pyQuant/CRex_Backups/RHRS/cutfile/LargeDataset/Sieve._21754_p4.f51
+../../trans  /home/newdriver/pyQuant/CRex_Backups/RHRS/cutfile/LargeDataset/Sieve._21755_p4.f51
+../../trans  /home/newdriver/pyQuant/CRex_Backups/RHRS/cutfile/LargeDataset/Sieve._21756_p4.f51
+../../trans  /home/newdriver/pyQuant/CRex_Backups/RHRS/cutfile/LargeDataset/Sieve._21757_p4.f51
+../../trans  /home/newdriver/pyQuant/CRex_Backups/RHRS/cutfile/LargeDataset/Sieve._21767_p4.f51
+../../trans  /home/newdriver/pyQuant/CRex_Backups/RHRS/cutfile/LargeDataset/Sieve._21768_p4.f51
+../../trans  /home/newdriver/pyQuant/CRex_Backups/RHRS/cutfile/LargeDataset/Sieve._21769_p4.f51
+../../trans  /home/newdriver/pyQuant/CRex_Backups/RHRS/cutfile/LargeDataset/Sieve._21770_p4.f51
+../../trans  /home/newdriver/pyQuant/CRex_Backups/RHRS/cutfile/LargeDataset/Sieve._21771_p4.f51
+../../trans  /home/newdriver/pyQuant/CRex_Backups/RHRS/cutfile/LargeDataset/Sieve._21772_p4.f51
+../../trans  /home/newdriver/pyQuant/CRex_Backups/RHRS/cutfile/LargeDataset/Sieve._21781_p4.f51
+../../trans  /home/newdriver/pyQuant/CRex_Backups/RHRS/cutfile/LargeDataset/Sieve._21782_p4.f51
+../../trans  /home/newdriver/pyQuant/CRex_Backups/RHRS/cutfile/LargeDataset/Sieve._21783_p4.f51
+../../trans  /home/newdriver/pyQuant/CRex_Backups/RHRS/cutfile/LargeDataset/Sieve._21784_p4.f51
+../../trans  /home/newdriver/pyQuant/CRex_Backups/RHRS/cutfile/LargeDataset/Sieve._21785_p4.f51
